f507f3e4f199a1faa8f415394bd80b738940fdf8
tobiasnf <tobiasnf@gmail.com>
2020-06-07 13:34:05 +0200

    table

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 
+let grid = document.querySelector('#table')
 
 async function hent() {
     let response = await fetch('https://api.spacexdata.com/v3/rockets');
     let data = await response.json();
-    console.log(data);    
+    console.log(data);
+
+    data.forEach(rocket => {
+        console.log('rocket', rocket)
+        let s = [rocket.rocket_name, rocket.height.meters, rocket.mass.kg]
+        grid.innerHTML += '<tr> 0 </tr>'
+/*         s.forEach(t => {
+            grid.innerHTML += '<td>' + t + '</td>'
+        }) */
+        //grid.innerHTML += '</tr>'
+    })
 }
 
 hent();
