fcaa9e360d19d8051fe564362e3a6fdb9ffc1187
tobiasnf <tobiasnf@gmail.com>
2020-06-07 14:54:16 +0200

    test

--- app.js
+++ app.js
@@ -25,12 +25,34 @@ async function hent() {
     data = await response.json();
     console.log(data);
 
-    let d = new Date();
-    let c = new Date(data.launch_date_utc);
-    console.log((c.getTime() - d.getTime())/ (1000 * 60 * 60 * 24))
+    updateTimer(data)
+    setInterval(() => {
+        updateTimer(data)
+    }, 1000);
+
+    document.querySelector('#missionName').innerHTML = data.mission_name;
+    document.querySelector('#rocketName').innerHTML = 'Rocket type: ' + data.rocket.rocket_name;
+    document.querySelector('#details').innerHTML = data.details;
+    document.querySelector('#image').src = data.links.mission_patch_small;
 
     loading.style.display = "none";
     grid.style.visibility = "visible";
+}
+
+function updateTimer(data) {
+    let d = new Date();
+    let c = new Date(data.launch_date_utc);
+    let miliSec = c - d;
+    let day = Math.floor(miliSec / (1000 * 60 * 60 * 24));
+    let tim = Math.floor(miliSec / (1000 * 60 * 60));
+    let min = Math.floor(miliSec / (1000 * 60));
+    let sec = Math.floor(miliSec / 1000);
+
+    document.querySelector('#timer').innerHTML =
+        day + ':' +
+        (tim - (day * 24)) + ':' +
+        (min - (tim * 60)) + ':' +
+        (sec - (min * 60));
 }
 
 hent();
