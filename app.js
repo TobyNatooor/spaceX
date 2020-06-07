e2f4c0b0dae374fed0bd6fea5467dc4f7dbe84f4
tobiasnf <tobiasnf@gmail.com>
2020-06-07 14:19:43 +0200

    test

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 let grid = document.querySelector('#table')
+let loading = document.querySelector('#loading')
 
 async function hent() {
     let response = await fetch('https://api.spacexdata.com/v3/rockets');
@@ -8,17 +9,28 @@ async function hent() {
 
     data.forEach(rocket => {
         console.log('rocket', rocket)
-        let s = [rocket.rocket_name, rocket.height.meters, rocket.mass.kg]
+        let s = [rocket.rocket_name, rocket.height.meters, rocket.mass.kg, '<a href=' + rocket.wikipedia + '>wikipedia</a>']
 
         let h = '<tr>'
-         s.forEach(t => {
+        s.forEach(t => {
             h += '<td>' + t + '</td>'
-        }) 
+        })
 
         h += '</tr>'
 
         grid.innerHTML += h
     })
+
+    response = await fetch('https://api.spacexdata.com/v3/launches/next');
+    data = await response.json();
+    console.log(data);
+
+    let d = new Date();
+    let c = new Date(data.launch_date_utc);
+    console.log((c.getTime() - d.getTime())/ (1000 * 60 * 60 * 24))
+
+    loading.style.display = "none";
+    grid.style.visibility = "visible";
 }
 
 hent();
